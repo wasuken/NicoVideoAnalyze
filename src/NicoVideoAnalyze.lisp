4c87cc6129b99c4a0a2138c10a7dfa008137b430
wasuken <wevorence@gmail.com>
2018-03-17 21:21:51 +0900

    add aggregate func. next, graph

--- src/NicoVideoAnalyze.lisp
+++ src/NicoVideoAnalyze.lisp
@@ -2,6 +2,12 @@
 ;;; 情報を取るタイミング
 (defvar *nico-get-idle-time* (* 60 60))
 
+;;; なぜ標準関数にないのか
+(defun take (lst n)
+  (if (or (zerop n) (null lst))
+      nil
+      (cons (car lst) (take (cdr lst) (1- n)))))
+
 ;;; 基礎URI
 ;;; これ固定で当分やっていき
 (defvar *nico-base-uri-format*
@@ -41,33 +47,29 @@
 ;;; database path
 (defvar *db-path* "../nico.sqlite3")
 
+
+;;; selectの抽象化
+(defun something-select-records (db-name select)
+  (dbi:with-connection (conn :sqlite3 :database-name db-name)
+    (let* ((query (dbi:prepare conn select))
+           ;; こんなアホなことするくらいならデータ構造変えたほうがよくね・・・？
+           ;; かえることはできませんでした。現実は非常である。
+           (result (dbi:execute query))
+           (rows '()))
+      ;; 存在しなかったらnilのまま
+      (loop for row = (dbi:fetch result)
+         while row
+         do (setf rows (cons row rows)))
+      rows)))
 ;;; 一度でも取得した動画の情報
 (defun nico-video-items (db-name)
-  (dbi:with-connection (conn :sqlite3 :database-name db-name)
-    (let* ((query (dbi:prepare conn "SELECT * FROM nico_video_item order by title"))
-           ;; こんなアホなことするくらいならデータ構造変えたほうがよくね・・・？
-           ;; かえることはできませんでした。現実は非常である。
-           (result (dbi:execute query))
-           (rows '()))
-      ;; 存在しなかったらnilのまま
-      (loop for row = (dbi:fetch result)
-         while row
-         do (setf rows (cons row rows)))
-      rows)))
-
+  (something-select-records db-name "select * from nico_video_item;"))
 ;;; joinしたレコード
 (defun nico-video-detail-items (db-name)
-  (dbi:with-connection (conn :sqlite3 :database-name db-name)
-    (let* ((query (dbi:prepare conn "SELECT * FROM nico_video_item as ni join nico_video_detail as nd on ni.content_id = nd.content_id order by title"))
-           ;; こんなアホなことするくらいならデータ構造変えたほうがよくね・・・？
-           ;; かえることはできませんでした。現実は非常である。
-           (result (dbi:execute query))
-           (rows '()))
-      ;; 存在しなかったらnilのまま
-      (loop for row = (dbi:fetch result)
-         while row
-         do (setf rows (cons row rows)))
-      rows)))
+  (something-select-records db-name "SELECT * FROM nico_video_item as ni join nico_video_detail as nd on ni.content_id = nd.content_id order by title;"))
+;;; はいくそー
+(defun nico-video-aggr (db-name)
+  (something-select-records db-name "SELECT ni.content_id,title,count(*) as aggr FROM nico_video_item as ni join nico_video_detail as nd on ni.content_id = nd.content_id group by ni.content_id order by aggr;"))
 
 ;;; 挿入
 ;;; 前DB系書いた時マクロつくって楽しようとしたらクッソ時間かかったから
@@ -96,7 +98,8 @@
       (loop for row = (dbi:fetch result)
          while row
          do (setf rows (cons row rows)))
-      (print rows)
+      ;; debug point
+      (if rows (print rows))
       (when (not rows)
         (insert-nico-video-item id (nth 0 item) db-name))
       (insert-nico-video-detail id
